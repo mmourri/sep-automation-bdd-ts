e784cdf64ea0b65eb657065b1825d5f52c67b117
mmourri <mmourri@gmail.com>
2026-08-31 15:31:36 -0700

    sep11 complete

--- steps/getting-started/startAndRefundDates.ts
+++ steps/getting-started/startAndRefundDates.ts
@@ -3,23 +3,24 @@ import { expect} from "@playwright/test";
 import { startApplicationPage, page } from "../../globalPagesSetup";
 import { productInfo } from "../../utilities/qa-data-reader";
 
-
-Then('the program start date is displayed', async function () {
-    await expect(startApplicationPage.programStartDate).toBeVisible();
+Then("the program start date in displayed", async function () {
+  await expect(startApplicationPage.programStartDate).toBeVisible();
 });
 
-Then('the program refund date is displayed', async function () {
-    await expect(startApplicationPage.refundEndDate).toBeVisible();
+Then("the program refund date in displayed", async function () {
+  await expect(startApplicationPage.refundEndDate).toBeVisible();
 });
 
-Then('the displayed start date for the program is correct', async function () {
-    const ACTUAL_START_DATE = await startApplicationPage.programStartDate.innerText();
-    const EXPECTED_START_DATE = productInfo.startDate;
-    expect(ACTUAL_START_DATE).toBe(EXPECTED_START_DATE);
+Then("the displayed start date for the program is correct", async function () {
+  const ACTUAL_START_DATE =
+    await startApplicationPage.programStartDate.innerText();
+  const EXPECTED_START_DATE = productInfo.startDate;
+  expect(ACTUAL_START_DATE).toBe(EXPECTED_START_DATE);
 });
 
-Then('the displayed refund date for the program is correct', async function () {
-    const ACTUAL_REFUND_DATE = await startApplicationPage.refundEndDate.innerText();
-    const EXPECTED_REFUND_DATE = productInfo.refundDate;
-    expect(ACTUAL_REFUND_DATE).toBe(EXPECTED_REFUND_DATE);
+Then("the displayed refund date for the program is correct", async function () {
+  const ACTUAL_REFUD_DATE =
+    await startApplicationPage.refundEndDate.innerText();
+  const EXPECTED_REFUD_DATE = productInfo.refundDate;
+  expect(ACTUAL_REFUD_DATE).toBe(EXPECTED_REFUD_DATE);
 });
