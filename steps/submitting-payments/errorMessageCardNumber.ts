c6525c893a658cfeb5409d8cffce01281f42431f
mmourri <mmourri@gmail.com>
2026-08-31 14:38:26 -0700

    sep 25 complete

--- steps/submitting-payments/errorMessageCardNumber.ts
+++ steps/submitting-payments/errorMessageCardNumber.ts
@@ -2,3 +2,15 @@ import { Given, Then, When } from "@cucumber/cucumber";
 import { expect} from "@playwright/test";
 import { reviewPaymentPage, page } from "../../globalPagesSetup";
 import { productInfo } from "../../utilities/qa-data-reader";
+
+When("user enters {string} card number", async function (string) {
+  await reviewPaymentPage.enterCardNumber(string);
+});
+
+When("user clicks the terms and conditions checkbox", async function () {
+  await reviewPaymentPage.clickTermsAndConditionsCheckbox();
+});
+
+Then("user should see the error message {string}", async function (string) {
+  await expect(reviewPaymentPage.cardNumberErrorMessage).toHaveText(string);
+});
